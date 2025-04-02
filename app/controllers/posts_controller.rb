a083677ad091e8abcc9caa63d2e2467ab0c4e89f
Tani <mizutani2908@yahoo.co.jp>
2025-04-02 23:17:34 +0900

    add_image

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,27 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-  end
-
-  def post_form
     
   end
 
+  def post_form
+    if @current
+
+    else
+      flash[:notice] = "ログインしてください"
+      redirect_to("/login_form")
+    end
+  end
+
   def post
-    @new = Post.new(text: params[:text], user_id: params[:id])
+    @new = Post.new(text: params[:text], user_id: @current.id)
     @new.save
+    redirect_to("/")
+  end
+
+  def delete
+    @delete = Post.find_by(id: params[:id])
+    @delete.destroy
     redirect_to("/")
   end
 end
