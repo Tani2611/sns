f289bb386627436fa99c9100087eec738510768f
Tani <mizutani2908@yahoo.co.jp>
2025-04-10 20:29:16 +0900

    add_search

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
     @follows = Follow.all
   end
 
-  def post_form
+  def new
     if @current
 
     else
@@ -14,9 +14,26 @@ class PostsController < ApplicationController
     end
   end
 
-  def post
+  def create
     @new = Post.new(text: params[:text], user_id: @current.id)
     @new.save
+    redirect_to("/")
+  end
+
+  def search
+    @searchs = Post.where("text LIKE (?)", "%#{params[:search]}%")
+  end
+
+  def edit
+    @user_post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @user_post = Post.find_by(id: params[:id])
+    @user_post.text = params[:text]
+    @user_post.save
+
+    flash[:notice] = "編集完了"
     redirect_to("/")
   end
 
